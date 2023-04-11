9e04020330dcdd53e7cfc85f6eb075192cb7af98
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-11 23:49:50 +0300

    готова задача 11

---
+++ task11/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = new Random().Next(100, 1000); // 99+1
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+int deleteCenter = DeleteCenter(number);
+int DeleteCenter(int num) // num  = number
+{
+    int first = num / 100;
+    int third = num % 10;
+    int deleteCenter = first * 10 + third;
+    return deleteCenter;
+}
+
+Console.WriteLine($"двузначное число = {deleteCenter}");
+
+
+// int number = new Random().Next(100, 1000); // 99+1
+// Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+// int firstDigit = number / 100;
+// int secondDigit = number % 10;
+// Console.WriteLine($"первое и третье число = {firstDigit} и {secondDigit}");
