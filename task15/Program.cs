3178972948ba3d8918e2ce4b0508a72b5a1c4bae
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-12 20:12:13 +0300

    готова задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number >0 && number <= 7)
+Console.WriteLine(number >0 && number < 6 ? "нет" : "да");
+
+
