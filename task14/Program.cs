54540ed97228c86aa258362b100cda7ee958b076
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-12 14:48:08 +0300

    готова задача 14

---
+++ task14/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(0, 1000);
+Console.WriteLine($"введите число -> {number}");
+
+
+
+int result = Result(number);
+if(result == 0)
+Console.WriteLine("-> да");
+else
+Console.WriteLine("-> нет");
+
+int Result(int num)
+{
+    int result = number %(7 * 23);
+    return result;
+}
