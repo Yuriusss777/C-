3d48c2a6e1c7408037db93600829c5a188c837d0
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-12 14:13:03 +0300

    готова задача 12

---
+++ task12/Program.cs
@@ -0,0 +1,22 @@
+﻿int firstNumber = new Random().Next(0, 1000);
+Console.WriteLine($"первое число -> {firstNumber}");
+int secondNumber = new Random().Next(0, 1000);
+Console.WriteLine($"второе число -> {secondNumber}");
+
+int theRemainderOfTheDivision = TheRemainderOfTheDivision(firstNumber);
+if(theRemainderOfTheDivision == 0)
+Console.WriteLine("-> кратно");
+else
+Console.WriteLine($"->не кратно, остаток {theRemainderOfTheDivision}");
+
+int TheRemainderOfTheDivision(int num)
+{
+    int theRemainderOfTheDivision = firstNumber %secondNumber;
+    return theRemainderOfTheDivision;
+}
+
+
+
+
+
+
