92c8228b2fb90237b796ed05ab737f3f049f8694
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-12 18:17:56 +0300

    готова задача 10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = Result(number);
+
+if (number < 0)
+number = -number; 
+if(number >= 100&&number <= 999)
+Console.WriteLine(result);
+
+int Result(int num)
+{
+    return(result = (num / 10)%10);
+}
