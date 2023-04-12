a07d51f36f8cde9c1074d0036faa461bccdae350
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-12 16:57:37 +0300

    готова задача 16

---
+++ task16/Program.cs
@@ -0,0 +1,16 @@
+﻿int firstNumber = new Random().Next(0, 1000);
+Console.WriteLine($"первое число -> {firstNumber}");
+int secondNumber = new Random().Next(0, 1000);
+Console.WriteLine($"второе число -> {secondNumber}");
+
+int squareNumber = SquareNumber(firstNumber);
+if(squareNumber == secondNumber)
+Console.WriteLine("-> да");
+else
+Console.WriteLine("нет");
+
+int SquareNumber(int num)
+{
+    int squareNumber = firstNumber * firstNumber;
+    return squareNumber;
+}
