bc32df756bd233afbe31f809ed652ba0d5948daf
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-16 20:31:21 +0300

    решение задачи 20

---
+++ task20/Program.cs
@@ -0,0 +1,46 @@
+﻿// double d = Math.Sqrt(5);
+
+// double d1 = 5.09987654;
+// double d1Round = Math.Round(d1, 2, MidpointRounding.ToZero);
+// Console.WriteLine(d1Round);
+
+
+// Console.WriteLine("Введите координаты точки А");
+// Console.Write("x1: ");
+// int xCoordinateA1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("y1: ");
+// int yCoordinateA1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты точки В");
+// Console.Write("x2: ");
+// int xCoordinateA2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("y2: ");
+// int yCoordinateA2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки А");
+Console.Write("x1: ");
+int xACoordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int yACoordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В");
+Console.Write("x2: ");
+int xBCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int yBCoordinate = Convert.ToInt32(Console.ReadLine());
+
+
+double distans = Distans(xACoordinate, yACoordinate, xBCoordinate, yBCoordinate);
+double distansRound = Math.Round(distans, 2, MidpointRounding.ToZero);
+Console.WriteLine(distansRound);
+
+double Distans(int x1, int y1, int x2, int y2)
+{
+//    int deltaX = x1 - x2;
+//    int deltaY = y1 - y2; 
+   double distans = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+// double distans = deltaX * deltaX + deltaY * deltaY;
+   return distans;
+}
+
+
+
