d8dd6fc72d6a348d0343c75badeaa9bdde25c0af
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-16 21:17:10 +0300

    решение задачи 22

---
+++ task22/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void TableAllSquares(int num)
+{
+   int index = 1; 
+   while (index < number)
+   {
+    Console.WriteLine($"{index, 3} {index * index, 4}");
+    index++;
+   }
+}
+
+
+TableAllSquares(number);
