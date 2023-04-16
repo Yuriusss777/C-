c000d97d96abfaedb5b9595e719c72eeb369683d
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-16 17:57:44 +0300

    решение задачи 18

---
+++ task18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер четверти оси координат: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+
+string coordinatAxis = CoordinatAxis(quarter);
+Console.WriteLine(coordinatAxis == null ? "Неправильный номер четверти" : coordinatAxis);
+
+string CoordinatAxis(int q)
+{
+    if (quarter == 1) return "x > 0 && y > 0";
+    if (quarter == 2) return "x < 0 && y > 0";
+    if (quarter == 3) return "x < 0 && y < 0";
+    if (quarter == 4) return "x > 0 && y < 0";
+    return null;
+}
