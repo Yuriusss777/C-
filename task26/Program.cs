d37ab07ef15b58f6b2779d383f6777d1c6eeb6e9
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-18 22:34:43 +0300

    решение задачи 26

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantity = Quantity(number);
+Console.WriteLine($"количество цифр в числе = {quantity}");
+
+int Quantity(int num) // int num = number
+{
+    int count = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num /= 10;
+        count = i;
+    }
+    return count;
+}
