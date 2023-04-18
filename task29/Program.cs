1f9ccd0406555f3a745fa810cd01212a8ad793f1
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-19 01:45:53 +0300

    решение задачи 29

---
+++ task29/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите длину массива");
+int lengthArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = Array(lengthArray);
+PrintArray(arr);
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    Random random = new Random(); // опритмизация объектов random
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next (0, 1000);
+    }
+    return array;
+   }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
