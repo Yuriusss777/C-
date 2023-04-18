766dbd959314022204063c19109778f632c40fe4
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-19 01:38:45 +0300

    решение задачи 27

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+int sum = Sum(number);
+Console.WriteLine(sum);
+
+int Sum(int num) // int num = number
+{
+    int sum1 = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        sum1 = sum1 + num % 10;
+        num = num / 10;
+    }
+    return sum1;
+}
