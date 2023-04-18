023edece02d4569d9c119ca8ac1af9c8f8877d82
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-18 23:07:53 +0300

    решение задачи 28

---
+++ task28/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int prNumbers = PrNumbers(number);
+Console.WriteLine($"произедение чисел от 1 до {number} = {prNumbers}");
+
+
+int PrNumbers(int num) // int num = number
+{
+    int pr = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        pr = pr * i; //sum += i
+    }
+    return pr;
+}
