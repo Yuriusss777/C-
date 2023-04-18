681736962d227ad5df233696dc0f3dcf8f8598ed
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-19 01:06:29 +0300

    решение задачи 25

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе натуральное число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = Sum(number1, number2);
+Console.WriteLine(sum);
+
+
+int Sum(int num1, int num2)
+{
+    int sum1 = num1;
+    for (int i = 2; i <= num2; i++)
+    {
+        sum1 = sum1 * num1;
+    }
+    return sum1;
+    }
