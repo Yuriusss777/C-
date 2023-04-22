c478cfbe35b027680f39c7051a2a876fc46e1d61
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-22 19:44:11 +0300

    решение задачи 32

---
+++ task32/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] CreatArrayRndInt(int size, int min, int max) // создать массив со случайными числами с параметрами: размер массива, мин значение и макс. значение для диапозона
+{
+    int[] arr = new int[size]; // создаем массив с размером size
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++) //arr.Length - количество элементов созданного массива, можно и size использовать
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr; // возвращаем готовый массив
+}
+
+void PrintArray(int[] arr) // метод, который выводит в консоль результаты  
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+
+void InverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+int[] array = CreatArrayRndInt(5, -9, 9); // создаем метод с параметрами
+Console.Write(" [");
+PrintArray(array);
+Console.Write("]");
+InverseArray(array);
+Console.Write(" -> [");
+PrintArray(array);
+Console.Write("]");
+
+
+//обязательна последовательность обращения к методу и вывода в консоль
