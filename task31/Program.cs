b0340430828644723012055bb9eb2adb2c5d58b8
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-22 17:56:09 +0300

    решение задачи 31

--- task31/Program.cs
+++ task31/Program.cs
@@ -1,25 +1,25 @@
-﻿int[] CreatArrayRndInt(int size, int min, int max)
+﻿int[] CreatArrayRndInt(int size, int min, int max) // создать массив со случайными числами с параметрами: размер массива, мин значение и макс. значение для диапозона
 {
-    int[] arr = new int[size];
+    int[] arr = new int[size]; // создаем массив с размером size
     Random rnd = new Random();
-    for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++) //arr.Length - количество элементов созданного массива, можно и size использовать
     {
-        arr[1] = rnd.Next(min, max + 1);
+        arr[i] = rnd.Next(min, max + 1);
     }
-    return arr;
+    return arr; // возвращаем готовый массив
 }
 
-void PrintArray(int[] arr)
+void PrintArray(int[] arr) // метод, который выводит в консоль результаты  
 {
     for (int i = 0; i < arr.Length; i++)
     {
         if (i < arr.Length - 1) Console.Write($"{arr[1]}, ");
-        else Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
     }
 }
 
-int[] array = CreatArrayRndInt(12, -9, 9);
-Console.Write("[");
+int[] array = CreatArrayRndInt(12, -9, 9); // создаем метод с параметрами
+PrintArray(array);
+Console.Write(" -> [");
 PrintArray(array);
 Console.Write("]");
-Console.WriteLine();
