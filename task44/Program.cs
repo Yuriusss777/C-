d290b53aa335fd0114b9f076c9d68d91ed7fe369
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-27 15:03:57 +0300

    решение задачи 44

---
+++ task44/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] fibonacci = Fibonacci(number);
+PrintArray(fibonacci);
+
+
+int[] Fibonacci(int size)
+{
+    int[] arr = new int[size];
+    arr[1] = 1;
+    for (int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr) 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
