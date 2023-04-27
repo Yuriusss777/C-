56d0846b9253d6b77fb960c3e1ddbec43a3d079c
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-27 22:57:53 +0300

    решение задачи 43

--- task43/Program.cs
+++ task43/Program.cs
@@ -1,50 +1,32 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-// 0, 7, 8, -2, -2 -> 2
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// -1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите значение b1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите необходимое количество чисел: ");
-int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number > 0)
+Console.WriteLine("Введите значение k1: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2: ");
+int number4 = Convert.ToInt32(Console.ReadLine());
+
+double x = X(number1, number2, number3, number4);
+double y = Y(number1, number2, number3, number4, x);
+Console.WriteLine($"{x}; {y}");
+
+double X(double num1, double num2, double num3, double num4)
 {
-    int[] newArr = Array(number);
-    int positivNumber = PositivNumber(newArr);
-    Console.WriteLine($"введенных чисел больше 0 = {positivNumber}");
-    PrintArray(newArr);
-}
-else Console.WriteLine("ввели неверное число");
-
-
-int[] Array(int num)
-{
-    int[] arr = new int[num];
-
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.WriteLine($"Введите элемент c индексом {i}: ");
-        arr[i] = Convert.ToInt32(Console.ReadLine());
-
-    }
-    return arr;
+    double x = -(num1 - num2) / (num3 - num4);
+    return x;
 }
 
-void PrintArray(int[] arr)
+double Y(double num1, double num2, double num3, double num4, double x)
 {
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-        else Console.Write($"{arr[i]}");
-    }
-}
-
-int PositivNumber(int[] newArr)
-{
-    int count = 0;
-    for (int i = 0; i < newArr.Length; i++)
-    {
-        if (newArr[i] > 0) count += 1;
-    }
-    return count;
+    double y = num4 * x + num2;
+    return y;
 }
