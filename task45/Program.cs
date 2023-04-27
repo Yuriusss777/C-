f2972862898e94ab2357659fbb720c8c15c91015
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-27 16:01:03 +0300

    решение задачи 45

---
+++ task45/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreatArrayRndInt(int size, int min, int max) // создать массив со случайными числами с параметрами: размер массива, мин значение и макс. значение для диапозона
+{
+    int[] arr = new int[size]; // создаем массив с размером size
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++) //arr.Length - количество элементов созданного массива, можно и size использовать
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr; // возвращаем готовый массив
+}
+
+void PrintArray(int[] arr) // метод, который выводит в консоль результаты  
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int[] CreateCopyArray(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    // newArr[newArr.Length - 1] = 10;
+    return newArr;
+}
+
+int[] array = CreatArrayRndInt(5, -9, 9);
+PrintArray(array);
+Console.WriteLine();
+int[] newArray = CreateCopyArray(array);
+PrintArray(newArray);
