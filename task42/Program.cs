13b562213e75fe484a611b411d14865afe23a99a
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-27 13:52:32 +0300

    решение задачи 42

---
+++ task42/Program.cs
@@ -0,0 +1,31 @@
+﻿// напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int resultBin = DecTobin(number);
+    Console.WriteLine(resultBin);
+}
+else Console.WriteLine("ввели неверное число");
+
+
+int DecTobin(int num)
+{
+    int result = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        result += num % 2 * count;
+        count = count * 10;
+        // count = num % 2;
+        // result = result * 10 + count;
+        num /= 2;
+    }
+    return result;
+}
+
