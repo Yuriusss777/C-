45180088acb38d9566ebab6238fb968dea96dd4c
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-04-27 10:30:06 +0300

    решение задачи 40

---
+++ task40/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите сторону А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону С: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+bool IsExistTriangle(int numA, int numB, int numC)
+{
+    if (numA < numB + numC && numB < numA + numC && numC < numA + numB)
+        return true;
+    else return false;
+}
+
+bool existTriangle = IsExistTriangle(numberA, numberB, numberC);
+Console.WriteLine(existTriangle ? "Такой треугольник существует" : "Такого треугольника нет");
+
