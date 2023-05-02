6a349cc07a28be84de8c80f6e7526c2f414da6d4
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-05-02 11:30:30 +0300

    решение задачи 51

---
+++ task51/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] CreatMatrixRndInt(int rows, int columns, int min, int max) // двумерный массив обозначается одной запятой, 
+// rows - обазначили размер массива х, columns - обазначили размер массива y
+{
+    int[,] matrix = new int[rows, columns]; // создаем массив с размером size
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) //GetLength(0) - измерение количества строк, 0 - это нулевой индекс массива
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+int DiagonalSum(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+                sum += matrix[i, j];
+        }
+    }
+    return (sum);
+}
+
+void Printmatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] array2d = CreatMatrixRndInt(3, 4, 0, 10);
+int diagonalSum = DiagonalSum(array2d);
+Printmatrix(array2d);
+Console.WriteLine();
+Console.Write($"Сумма индексов основоной диагонали = {diagonalSum}");
