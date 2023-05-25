7c6f344f3de8945c568f95a73c8bfb9ebeff2748
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-05-25 17:51:00 +0300

    решение 67

---
+++ task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigits(number);
+Console.WriteLine(sumDigits);
+
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    return num%10 + SumDigits(num / 10);
+}
+
+
