a8c0291feeeb9b14561d01a5458a558b6d85ce91
«Yuriy» <dolzhenkov.yury@mail.ru>
2023-05-25 17:11:27 +0300

    решение 65

---
+++ task65/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+Console.WriteLine("Введите целое положительное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(numberM, numberN);
+
+void NaturalNumbers(int numM, int numN)
+{
+    if (numM < numN)
+    {
+        Console.Write($"{numM} ");
+        NaturalNumbers(numM + 1, numN);
+    }
+    else if (numM > numN)
+    {
+        Console.Write($"{numN} ");
+        NaturalNumbers(numN + 1, numM);
+    }
+    else Console.Write(numM);
+}
